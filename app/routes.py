ccc772fafee98b324c9c3bd8413bf8a1bf19efc3
Yiwen Li <detectivelyw@gmail.com>
2020-01-10 15:42:56 -0500

    updates.

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Yiwen'}
+    user = {'username': 'Miguel'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -17,3 +18,7 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
